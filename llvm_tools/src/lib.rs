234af0de32a1a75c7ee577a5ee3129c7e0912d0c
Liam Eckert <liameckert17@gmail.com>
2025-06-12 01:15:18 -0400

    Deleted instr_histogram and llvm_module, update lib.rs

--- llvm_tools/src/lib.rs
+++ llvm_tools/src/lib.rs
@@ -1,30 +1,17 @@
-mod llvm_module;
-mod bb_stats;
-mod function_stats;
-mod module_stats;
+mod mod_features;
+mod fn_features;
+mod bb_features;
 
-use pyo3::{prelude::*, types::PyBytes};
+use pyo3::prelude::*;
 
-use llvm_module::LLVMModule;
-use bb_stats::BBStats;
-use function_stats::FunctionStats;
-use module_stats::ModuleStats;
-
-#[pyfunction]
-fn bitcode_to_ir(bitcode: Bound<PyBytes>) -> PyResult<String> {
-    let module = LLVMModule::from_bitcode(bitcode.as_bytes())
-        .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(e.to_string()))?;
-    let ir = module
-        .to_ir()
-        .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(e.to_string()))?;
-    Ok(ir)
-}
+use mod_features::ModFeatures;
+use fn_features::FnFeatures;
+use bb_features::BBFeatures;
 
 #[pymodule]
 fn llvm_tools(_py: Python, m: Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_wrapped(wrap_pyfunction!(bitcode_to_ir))?;
-    m.add_class::<BBStats>()?;
-    m.add_class::<FunctionStats>()?;
-    m.add_class::<ModuleStats>()?;
+    m.add_class::<ModFeatures>()?;
+    m.add_class::<FnFeatures>()?;
+    m.add_class::<BBFeatures>()?;
     Ok(())
 }
